3e89489ffb8e663ccd4177391eccf69bb9804096
Alexey <alekseiovechkin@gmail.com>
2019-07-30 00:36:36 +0300

    added folder Lesson1

---
+++ Lesson1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+
+	public static void main(String[] args) {
+		byte core = 4;
+		short multiplier = 34;
+		int fsb = 24000000;
+		long frequency = 3400000000L;
+		float cpuVoltage = 1.2368f;
+		double pi = 3.141592653589d;
+		char tm = 'i';
+		boolean isExist = true;
+		System.out.println("core = " + core);
+		System.out.println("multiplier = " + multiplier);
+		System.out.println("fsb = " + fsb);
+		System.out.println("frequency = " + frequency);
+		System.out.println("cpuVoltage = " + cpuVoltage);
+		System.out.println("tm = " + tm);
+		System.out.println("isExist = " + isExist);
+		System.out.println("pi = " + pi);
+	}
+}

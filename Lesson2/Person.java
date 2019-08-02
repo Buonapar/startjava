24a7e1a4eaf060a71e6fd7377f7a63929ad280e9
Alexey <alekseiovechkin@gmail.com>
2019-08-02 16:18:28 +0300

    add file Person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	boolean isMale = true;
+	String name = "Ivan";
+	float growth = 1.90f;
+	int weight = 90;
+	int age = 45;
+
+	void go() {
+		System.out.println("Человек идет");
+	}
+
+	void sit() {
+		System.out.println("Человек сидит");
+	}
+
+	void run() {
+		System.out.println("Человек бежит");
+	}
+
+	void speak() {
+		System.out.println("Человек говорит");
+	}
+
+	void learn() {
+		System.out.println("Человек учит Java");
+	}
+
+}
